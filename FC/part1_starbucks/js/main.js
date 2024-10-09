0c9ed251b8894fffc6512e740d2e7518003b7bad
jeongho <leejh9128@gmail.com>
2024-10-09 12:56:06 +0900

    FC Part1 : Header - 배지 추가
    배지 및 다음 기능 추가
    1. 스크롤 시 부하를 막기위한 lodash
    2. 자연스러운 애니메이션 효과를 위한 gsap

--- FC/part1_starbucks/js/main.js
+++ FC/part1_starbucks/js/main.js
@@ -14,3 +14,29 @@ searchInputEl.addEventListener('blur', function() {
     searchEl.classList.remove('focused');
     searchInputEl.setAttribute('placeholder', '');
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+// window를 스크롤할 때마다 기본적으로는 실행됨.(부하 문제)
+// lodash의 throttle을 사용해서 300밀리세컨드마다 실행되게 수정.
+// _.throttle(함수, 시간)
+
+// GSAP을 사용해서 애니메이션 효과를 자연스럽게 추가.
+// gsap.to(요소, 지속시간(초), 옵션);
+window.addEventListener('scroll', _.throttle(function(){
+    if(window.scrollY > 500){
+        // 배지 숨기기
+        gsap.to(badgeEl, .6, {
+            opacity: 0,
+            display: 'none'
+        })
+    }
+    else{
+        // 배지 보이기
+        gsap.to(badgeEl, .6, {
+            opacity: 1,
+            display: 'block'
+        })
+    }
+}, 300));
