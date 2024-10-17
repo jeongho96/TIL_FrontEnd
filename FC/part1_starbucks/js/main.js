2bffcfd1363e9a7f9f96ee2db81b833637fe1d14
jeongho <leejh9128@gmail.com>
2024-10-17 11:49:47 +0900

    FC Part1 : 1차 마무리
    - 최상단 이동 버튼 추가(toTop)
    - 코드 1차 점검 및 마무리

--- FC/part1_starbucks/js/main.js
+++ FC/part1_starbucks/js/main.js
@@ -22,7 +22,7 @@ searchInputEl.addEventListener('blur', function () {
 })
 
 
-const badgeEl = document.querySelector('header .badges');
+
 
 // window를 스크롤할 때마다 기본적으로는 실행됨.(부하 문제)
 // lodash의 throttle을 사용해서 300밀리세컨드마다 실행되게 수정.
@@ -30,61 +30,90 @@ const badgeEl = document.querySelector('header .badges');
 
 // GSAP을 사용해서 애니메이션 효과를 자연스럽게 추가.
 // gsap.to(요소, 지속시간(초), 옵션);
-window.addEventListener('scroll', _.throttle(function(){
-    if(window.scrollY > 500){
-        // 배지 숨기기
-        gsap.to(badgeEl, .6, {
-            opacity: 0,
-            display: 'none'
-        })
-    }
-    else{
-        // 배지 보이기
-        gsap.to(badgeEl, .6, {
-            opacity: 1,
-            display: 'block'
-        })
-    }
-}, 300));
+/**
+ * 페이지 스크롤에 따른 요소 제어
+ */
+// 페이지 스크롤에 영향을 받는 요소들을 검색!
+const badgeEl = document.querySelector('header .badges')
+const toTopEl = document.querySelector('#to-top')
+// 페이지에 스크롤 이벤트를 추가!
+// 스크롤이 지나치게 자주 발생하는 것을 조절(throttle, 일부러 부하를 줌)
+window.addEventListener('scroll', _.throttle(function () {
+  // 페이지 스크롤 위치가 500px이 넘으면.
+  if (window.scrollY > 500) {
+    // Badge 요소 숨기기!
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    })
+    // 상단으로 스크롤 버튼 보이기!
+    gsap.to(toTopEl, .2, {
+      x: 0
+    })
 
-
-// visual 영역의 fade-in 순차처리
-const fadeEls = document.querySelectorAll('.visual .fade-in');
-
-fadeEls.forEach(function(fadeEl, index){
-    gsap.to(fadeEl, 1, {
-        delay: (index + 1) * .7, // 각 요소들이 순차적으로 0.7초마다 수행
-        opacity: 1
-    });
-});
-
-
-// swiper 코드 추가
-// new Swiper(선택자, 옵션)
-new Swiper('.notice-line .swiper-container', {
-    direction: 'vertical',
-    autoplay: true,
-    loop: true
-});
-new Swiper('.promotion .swiper-container', {
-    // direction: 'horizontal', // 수평 슬라이드
-    autoplay: { // 자동 재생 여부
-      delay: 5000 // 5초마다 슬라이드 바뀜
-    },
-    loop: true, // 반복 재생 여부
-    slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
-    spaceBetween: 10, // 슬라이드 사이 여백
-    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
-    pagination: { // 페이지 번호 사용 여부
-      el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
-      clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
-    },
-    navigation: { // 슬라이드 이전/다음 버튼 사용 여부
-      prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
-      nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
-    }
+  // 페이지 스크롤 위치가 500px이 넘지 않으면.
+  } else {
+    // Badge 요소 보이기!
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    })
+    // 상단으로 스크롤 버튼 숨기기!
+    gsap.to(toTopEl, .2, {
+      x: 100
+    })
+  }
+}, 300))
+// 상단으로 스크롤 버튼을 클릭하면,
+toTopEl.addEventListener('click', function () {
+  // 페이지 위치를 최상단으로 부드럽게(0.7초 동안) 이동.
+  gsap.to(window, .7, {
+    scrollTo: 0
   })
+})
 
+
+/**
+ * 순서대로 나타나는 기능
+ */
+// 나타날 요소들(.fade-in) 찾기.
+const fadeEls = document.querySelectorAll('.visual .fade-in')
+// 나타날 요소들을 하나씩 반복해서 처리!
+fadeEls.forEach(function (fadeEl, index) {
+  // 각 요소들을 순서대로(delay) 보여지게 함!
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7,
+    opacity: 1
+  })
+})
+
+
+/**
+ * 슬라이드 요소 관리
+ */
+new Swiper('.notice-line .swiper-container', {
+  direction: 'vertical', // 수직 슬라이드
+  autoplay: true, // 자동 재생 여부
+  loop: true // 반복 재생 여부
+})
+new Swiper('.promotion .swiper-container', {
+  // direction: 'horizontal', // 수평 슬라이드
+  autoplay: { // 자동 재생 여부
+    delay: 5000 // 5초마다 슬라이드 바뀜
+  },
+  loop: true, // 반복 재생 여부
+  slidesPerView: 3, // 한 번에 보여줄 슬라이드 개수
+  spaceBetween: 10, // 슬라이드 사이 여백
+  centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+  pagination: { // 페이지 번호 사용 여부
+    el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+    clickable: true // 사용자의 페이지 번호 요소 제어 가능 여부
+  },
+  navigation: { // 슬라이드 이전/다음 버튼 사용 여부
+    prevEl: '.promotion .swiper-prev', // 이전 버튼 선택자
+    nextEl: '.promotion .swiper-next' // 다음 버튼 선택자
+  }
+})
 new Swiper('.awards .swiper-container', {
   // direction: 'horizontal', // 수평 슬라이드
   autoplay: true, // 자동 재생 여부
@@ -99,9 +128,7 @@ new Swiper('.awards .swiper-container', {
 })
 
 
-
-  
-  /**
+/**
  * Promotion 슬라이드 토글 기능
  */
 // 슬라이드 영역 요소 검색!
@@ -122,7 +149,6 @@ promotionToggleBtn.addEventListener('click', function () {
     promotionEl.classList.remove('hide')
   }
 })
-
 
 
 /**
@@ -168,9 +194,6 @@ spyEls.forEach(function (spyEl) {
     .setClassToggle(spyEl, 'show') // 요소가 화면에 보이면 show 클래스 추가
     .addTo(new ScrollMagic.Controller()) // 컨트롤러에 장면을 할당(필수!)
 })
-
-
-
 
 
 /**
